de389a8cf5d6af7e6065021138025ea9eccd9067
vikram <vikrammudhiraj85@gmail.com>
2020-10-05 16:49:10 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -51,3 +51,35 @@
     });  
 
 })(jQuery);
+
+
+ // portfolio start
+ $(window).on("load", function() {
+  var $container = $('.awesome-project-content');
+  $container.isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 750,
+      easing: 'linear',
+      queue: false
+    }
+  });
+  var pro_menu = $('.project-menu li a');
+  pro_menu.on("click", function() {
+    var pro_menu_active = $('.project-menu li a.active');
+    pro_menu_active.removeClass('active');
+    $(this).addClass('active');
+    var selector = $(this).attr('data-filter');
+    $container.isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
+    });
+    return false;
+  });
+
+});
+//portfolio end
